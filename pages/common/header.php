8f0f70f7abd508bf7b976de63f645a89687d3ffd
Paul Hewson <paul@userdiven.co.nz>
2018-08-20 07:15:56 +1200

    Tidy up header

--- pages/common/header.php
+++ pages/common/header.php
@@ -1,14 +1,17 @@
 <header>
-	<nav class="blue">
+	<div class="navbar-fixed">
+		<nav class="blue">
 		<div class="nav-wrapper">
-			<div class="container">
-				<a href="#" class="brand-logo">
+			<div class="containerx">
+				<a href="#" class="brand-logo center">
 					CM Template
 				</a>
 				<a href="#" data-target="mobile-menu" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-				<ul class="right hide-on-med-and-down">
+				<ul class="left hide-on-med-and-down">
 					<li><a href="#">Option 1</a></li>
 					<li><a href="#">Option 2</a></li>
+				</ul>
+				<ul class="right hide-on-med-and-down">
 <?php if ($user->loggedIn()){
 	echo '<li><a href="?logout" id="aLogout">Logout</a></li>
 		<li><a href="#"><span class="new badge" data-badge-caption="'. $user->username() .'"></span></a></li>';
@@ -46,5 +49,19 @@
 				</ul>
 			</div>
 		</div>
-	</nav>
-</header>
+	</nav>			
+	</div>
+
+
+<section class="hide-on-small-only">
+	<div class="parallax-container">
+		<div class="parallax">
+			<img src="img\Lion.jpg" alt="" class="responsive-img">
+		</div>
+	</div>
+</section> 
+</header>	
+<section class="container">
+	<h1>The CM Sample Site</h1>
+	<p>This is a site for ....</p>
+</section>
